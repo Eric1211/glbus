1321af5c2a945830e03fc47daa255a74ff52ade2
wangrunqing <wangrunqing@navinfo.com>
2019-08-21 19:41:13 +0800

    fix

--- app/src/main/java/com/guanglun/glbus/MainActivity.java
+++ app/src/main/java/com/guanglun/glbus/MainActivity.java
@@ -1,25 +1,36 @@
 package com.guanglun.glbus;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.ComponentName;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.content.Intent;
 import android.app.FragmentManager;
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends FragmentActivity implements View.OnClickListener {
@@ -30,8 +41,13 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     public Intent mSettingIntent;
     public  Intent mNotificationIntent;
     public  Intent mFavoriteIntent;
-
     private busFragment frag2;
+
+
+    private final int SDK_PERMISSION_REQUEST = 127;
+    private ListView FunctionList;
+    private String permissionInfo;
+
 
     FragmentManager manager = getFragmentManager();
     Fragment frag3 = manager.findFragmentById(3);
@@ -82,10 +98,30 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		FunctionList = (ListView) findViewById(R.id.functionList);
+
+
+
+		List<Student> stuList=new ArrayList<>();
+		for(int i=0;i<10;i++){
+			Student stu=new Student();
+			stu.setAge(10+i);
+			stu.setName("name"+i);
+			stu.setPhoto(R.mipmap.ic_launcher);
+			stuList.add(stu);
+		}
+
+
+		MyAdapter adapter=new MyAdapter(stuList,MainActivity.this);
+		FunctionList.setAdapter(adapter);
+
+		// after andrioid m,must request Permiision on runtime
+		getPersimmions();
+		
         LogUtil.d("MainActivity process id is " + android.os.Process.myPid());
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+       mTextMessage = (TextView) findViewById(R.id.message);
+       BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         processExtraData();
@@ -99,6 +135,127 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         }
     }
 
+@TargetApi(23)
+	private void getPersimmions() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			ArrayList<String> permissions = new ArrayList<String>();
+			/***
+			 * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+			 */
+			// 定位精确位置
+			if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+				permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+			}
+			if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+				permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+			}
+			/*
+			 * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+			 */
+			// 读写权限
+			if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+				permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+			}
+			// 读取电话状态权限
+			if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
+				permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
+			}
+			
+			if (permissions.size() > 0) {
+				requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+			}
+		}
+	}
+
+	@TargetApi(23)
+	private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+		if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+			if (shouldShowRequestPermissionRationale(permission)){
+				return true;
+			}else{
+				permissionsList.add(permission);
+				return false;
+			}
+				
+		}else{
+			return true;
+		}
+	}
+
+	@TargetApi(23)
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		// TODO Auto-generated method stub
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		FunctionList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				// TODO Auto-generated method stub
+				Class<?> TargetClass = null;
+				switch (arg2) {
+				case 0:
+				//	TargetClass = LocationActivity.class;
+					break;
+				case 1:
+				//	TargetClass = LocationOption.class;
+					break;
+				case 2:
+				//	TargetClass = LocationAutoNotify.class;
+					break;
+				case 3:
+				//	TargetClass = LocationFilter.class;
+					break;
+				case 4:
+				//	TargetClass = NotifyActivity.class;
+					break;
+				case 5:
+				//	TargetClass = IndoorLocationActivity.class;
+					break;
+				case 6:
+                 //   TargetClass = IsHotWifiActivity.class;
+					break;
+				case 7:
+                  //  TargetClass = ForegroundActivity.class;
+					break;
+				case 8:
+				//	TargetClass = QuestActivity.class;
+					break;
+				default:
+					break;
+				}
+				if (TargetClass != null) {
+					Intent intent = new Intent(MainActivity.this, TargetClass);
+					intent.putExtra("from", 0);
+					startActivity(intent);
+				}
+			}
+		});
+	}
+
+	private List<String> getData() {
+
+		List<String> data = new ArrayList<String>();
+		data.add("基础定位功能");
+		data.add("配置定位参数");
+		data.add("自定义回调示例");
+		data.add("连续定位示例");
+		data.add("位置消息提醒");
+		data.add("室内定位功能");
+		data.add("判断移动热点");
+		data.add("android 8.0后台定位示例");
+		data.add("常见问题说明");
+
+		return data;
+	}
+	
     @Override
     protected void onDestroy()
     {
--- app/src/main/java/com/guanglun/glbus/favoriteActivity.java
+++ app/src/main/java/com/guanglun/glbus/favoriteActivity.java
@@ -1,16 +1,332 @@
 package com.guanglun.glbus;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class favoriteActivity extends AppCompatActivity {
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.utils.DistanceUtil;
+import com.guanglun.glbus.service.LocationService;
+import com.guanglun.glbus.service.Utils;
+
+import java.util.LinkedList;
+
+
+public class favoriteActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap;
+    private Button startLocation;
+    private LocationService locService;
+    private LinkedList<LocationEntity> locationList = new LinkedList<LocationEntity>(); // 存放历史定位结果的链表，最大存放当前结果的前5次定位结果
+    private LocationService locationService;
+    private TextView LocationResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_favorite);
-        processExtraData();
+
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        startLocation = (Button) findViewById(R.id.startlocation);
+        startLocation.setOnClickListener((View.OnClickListener) this);
+
+        LocationResult = (TextView) findViewById(R.id.address);
+        LocationResult.setMovementMethod(ScrollingMovementMethod.getInstance());
+
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.zoomTo(15));
+        locService = ((LocationApplication) getApplication()).locationService;
+        LocationClientOption mOption = locService.getDefaultLocationClientOption();
+        mOption.setLocationMode(LocationClientOption.LocationMode.Battery_Saving);
+        mOption.setCoorType("bd09ll");
+        locService.setLocationOption(mOption);
+        locService.registerListener(listener);
+        locService.start();
+        ;
+    }
+
+
+    /***
+     * 定位结果回调，在此方法中处理定位结果
+     */
+    BDAbstractLocationListener listener = new BDAbstractLocationListener() {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // TODO Auto-generated method stub
+
+            if (location != null && (location.getLocType() == 161 || location.getLocType() == 66)) {
+                Message locMsg = locHander.obtainMessage();
+                Bundle locData;
+                locData = Algorithm(location);
+                if (locData != null) {
+                    locData.putParcelable("loc", location);
+                    locMsg.setData(locData);
+                    locHander.sendMessage(locMsg);
+                }
+            }
+        }
+
+    };
+
+
+    @Override
+    public void onClick(View v) {
+
+        if (startLocation.getText().toString().equals(getString(R.string.startlocation))) {
+            locationService.start();// 定位SDK
+            // start之后会默认发起一次定位请求，开发者无须判断isstart并主动调用request
+            startLocation.setText(getString(R.string.stoplocation));
+        } else {
+            LocationResult.setText(getString(R.string.locationFailed));
+            locationService.stop();
+            startLocation.setText(getString(R.string.startlocation));
+        }
+    }
+
+    /***
+     * 平滑策略代码实现方法，主要通过对新定位和历史定位结果进行速度评分，
+     * 来判断新定位结果的抖动幅度，如果超过经验值，则判定为过大抖动，进行平滑处理,若速度过快，
+     * 则推测有可能是由于运动速度本身造成的，则不进行低速平滑处理 ╭(●｀∀´●)╯
+     *
+     * @param location
+     * @return Bundle
+     */
+    private Bundle Algorithm(BDLocation location) {
+        Bundle locData = new Bundle();
+        double curSpeed = 0;
+        if (locationList.isEmpty() || locationList.size() < 2) {
+            LocationEntity temp = new LocationEntity();
+            temp.location = location;
+            temp.time = System.currentTimeMillis();
+            locData.putInt("iscalculate", 0);
+            locationList.add(temp);
+        } else {
+            if (locationList.size() > 5)
+                locationList.removeFirst();
+            double score = 0;
+            for (int i = 0; i < locationList.size(); ++i) {
+                LatLng lastPoint = new LatLng(locationList.get(i).location.getLatitude(),
+                        locationList.get(i).location.getLongitude());
+                LatLng curPoint = new LatLng(location.getLatitude(), location.getLongitude());
+                double distance = DistanceUtil.getDistance(lastPoint, curPoint);
+                curSpeed = distance / (System.currentTimeMillis() - locationList.get(i).time) / 1000;
+                score += curSpeed * Utils.EARTH_WEIGHT[i];
+            }
+            if (score > 0.00000999 && score < 0.00005) { // 经验值,开发者可根据业务自行调整，也可以不使用这种算法
+                location.setLongitude(
+                        (locationList.get(locationList.size() - 1).location.getLongitude() + location.getLongitude())
+                                / 2);
+                location.setLatitude(
+                        (locationList.get(locationList.size() - 1).location.getLatitude() + location.getLatitude())
+                                / 2);
+                locData.putInt("iscalculate", 1);
+            } else {
+                locData.putInt("iscalculate", 0);
+            }
+            LocationEntity newLocation = new LocationEntity();
+            newLocation.location = location;
+            newLocation.time = System.currentTimeMillis();
+            locationList.add(newLocation);
+
+        }
+        return locData;
+    }
+
+    /***
+     * 接收定位结果消息，并显示在地图上
+     */
+    private Handler locHander = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+            super.handleMessage(msg);
+            try {
+                BDLocation location = msg.getData().getParcelable("loc");
+                int iscal = msg.getData().getInt("iscalculate");
+                if (location != null) {
+                    LatLng point = new LatLng(location.getLatitude(), location.getLongitude());
+                    // 构建Marker图标
+                    BitmapDescriptor bitmap = null;
+                    if (iscal == 0) {
+                        bitmap = BitmapDescriptorFactory.fromResource(R.drawable.icon_openmap_mark); // 非推算结果
+                    } else {
+                        bitmap = BitmapDescriptorFactory.fromResource(R.drawable.icon_openmap_focuse_mark); // 推算结果
+                    }
+
+                    // 构建MarkerOption，用于在地图上添加Marker
+                    OverlayOptions option = new MarkerOptions().position(point).icon(bitmap);
+                    // 在地图上添加Marker，并显示
+                    mBaiduMap.addOverlay(option);
+                    mBaiduMap.setMapStatus(MapStatusUpdateFactory.newLatLng(point));
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+    };
+
+
+    /*****
+     *
+     * 定位结果回调，重写onReceiveLocation方法，可以直接拷贝如下代码到自己工程中修改
+     *
+     */
+    private BDAbstractLocationListener mListener = new BDAbstractLocationListener() {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // TODO Auto-generated method stub
+            if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+                StringBuffer sb = new StringBuffer(256);
+                sb.append("time : ");
+                /**
+                 * 时间也可以使用systemClock.elapsedRealtime()方法 获取的是自从开机以来，每次回调的时间；
+                 * location.getTime() 是指服务端出本次结果的时间，如果位置不发生变化，则时间不变
+                 */
+                sb.append(location.getTime());
+                sb.append("\nlocType : ");// 定位类型
+                sb.append(location.getLocType());
+                sb.append("\nlocType description : ");// *****对应的定位类型说明*****
+                sb.append(location.getLocTypeDescription());
+                sb.append("\nlatitude : ");// 纬度
+                sb.append(location.getLatitude());
+                sb.append("\nlontitude : ");// 经度
+                sb.append(location.getLongitude());
+                sb.append("\nradius : ");// 半径
+                sb.append(location.getRadius());
+                sb.append("\nCountryCode : ");// 国家码
+                sb.append(location.getCountryCode());
+                sb.append("\nCountry : ");// 国家名称
+                sb.append(location.getCountry());
+                sb.append("\ncitycode : ");// 城市编码
+                sb.append(location.getCityCode());
+                sb.append("\ncity : ");// 城市
+                sb.append(location.getCity());
+                sb.append("\nDistrict : ");// 区
+                sb.append(location.getDistrict());
+                sb.append("\nStreet : ");// 街道
+                sb.append(location.getStreet());
+                sb.append("\naddr : ");// 地址信息
+                sb.append(location.getAddrStr());
+                sb.append("\nUserIndoorState: ");// *****返回用户室内外判断结果*****
+                sb.append(location.getUserIndoorState());
+                sb.append("\nDirection(not all devices have value): ");
+                sb.append(location.getDirection());// 方向
+                sb.append("\nlocationdescribe: ");
+                sb.append(location.getLocationDescribe());// 位置语义化信息
+                sb.append("\nPoi: ");// POI信息
+                if (location.getPoiList() != null && !location.getPoiList().isEmpty()) {
+                    for (int i = 0; i < location.getPoiList().size(); i++) {
+                        Poi poi = (Poi) location.getPoiList().get(i);
+                        sb.append(poi.getName() + ";");
+                    }
+                }
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                    sb.append("\nspeed : ");
+                    sb.append(location.getSpeed());// 速度 单位：km/h
+                    sb.append("\nsatellite : ");
+                    sb.append(location.getSatelliteNumber());// 卫星数目
+                    sb.append("\nheight : ");
+                    sb.append(location.getAltitude());// 海拔高度 单位：米
+                    sb.append("\ngps status : ");
+                    sb.append(location.getGpsAccuracyStatus());// *****gps质量判断*****
+                    sb.append("\ndescribe : ");
+                    sb.append("gps定位成功");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                    // 运营商信息
+                    if (location.hasAltitude()) {// *****如果有海拔高度*****
+                        sb.append("\nheight : ");
+                        sb.append(location.getAltitude());// 单位：米
+                    }
+                    sb.append("\noperationers : ");// 运营商信息
+                    sb.append(location.getOperators());
+                    sb.append("\ndescribe : ");
+                    sb.append("网络定位成功");
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                    sb.append("\ndescribe : ");
+                    sb.append("离线定位成功，离线定位结果也是有效的");
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    sb.append("\ndescribe : ");
+                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+                }
+                logMsg(sb.toString());
+            }
+        }
+
+    };
+
+
+    /**
+     * 显示请求字符串
+     *
+     * @param str
+     */
+    public void logMsg(String str) {
+        final String s = str;
+        try {
+            if (LocationResult != null) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        LocationResult.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                LocationResult.setText(s);
+                            }
+                        });
+
+                    }
+                }).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // -----------location config ------------
+        locationService = ((LocationApplication) getApplication()).locationService;
+        //获取locationservice实例，建议应用中只初始化1个location实例，然后使用，可以参考其他示例的activity，都是通过此种方式获取locationservice实例的
+        locationService.registerListener(mListener);
+        //注册监听
+        int type = getIntent().getIntExtra("from", 0);
+        if (type == 0) {
+            locationService.setLocationOption(locationService.getDefaultLocationClientOption());
+        } else if (type == 1) {
+            locationService.setLocationOption(locationService.getOption());
+        }
     }
 
     @Override
@@ -24,10 +340,20 @@ public class favoriteActivity extends AppCompatActivity {
 
     }
 
-    private void processExtraData(){
+    private void processExtraData() {
 
         Intent intent = getIntent();
         //use the data received here
+    }
+
+    /**
+     * 封装定位结果和时间的实体类
+     *
+     * @author baidu
+     */
+    class LocationEntity {
+        BDLocation location;
+        long time;
     }
 
 }
